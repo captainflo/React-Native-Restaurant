67c8d01ca72731849c507756539b73e44c3b3a02
Florian Lahitte <lahitte.florian@gmail.com>
2020-08-12 08:42:19 -0400

    add gif and readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# YELP-API (Course Project)
+
+Search for restaurants by menu items, food types, or cultural backgrounds based on location and time!
+
+**Preview**
+
+![](wiki.gif)
+
+**Technologies Used**
+
+**Backend**
+
+- Expo
+
+**Front-End**
+
+- react
+- axios
+- react-native
+- react-navigation
+
+**API**
+
+- Yelp
---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
+
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam('id');
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  text: {
+    fontSize: 20,
+    padding: 10,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    borderRadius: 8,
+    margin: 10,
+  },
+});
+
+export default ResultsShowScreen;
